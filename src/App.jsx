a353a43309d67dbb55e5a6e9cd1b766d78a62ff5
Bo-Yang-web <e1326035@u.nus.edu>
2024-10-09 15:04:57 +0800

    try7

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,78 @@ const initialTravellers = [
   },
 ];
 
+function TravellerRow(props) {
+  {/*Q3. Placeholder to initialize local variable based on traveller prop.*/}
+  return (
+    <tr>
+	  {/*Q3. Placeholder for rendering one row of a table with required traveller attribute values.*/}
+    <td>{props.traveller.id}</td>
+    <td>{props.traveller.name}</td>
+    <td>{props.traveller.phone}</td>
+    <td>{props.traveller.bookingTime.toLocaleString()}</td>   
+    </tr>
+  );
+}
+
+function Display(props) {
+
+	/*Q3. Write code to render rows of table, reach corresponding to one traveller. Make use of the TravellerRow function that draws one row.*/
+
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr>
+	  {/*Q3. Below table is just an example. Add more columns based on the traveller attributes you choose.*/}
+          <th>ID</th>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Booking Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/*Q3. write code to call the JS variable defined at the top of this function to render table rows.*/}
+        {props.travellers.map(traveller => (
+          <TravellerRow key={traveller.id} traveller={traveller} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+class Add extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    /*Q4. Fetch the passenger details from the add form and call bookTraveller()*/
+    const form = document.forms.addTraveller;
+    const maxId = Math.max(...this.props.travellers.map(t => t.id), 0);
+    this.props.bookTraveller({ 
+      id: maxId + 1,
+      name: form.travellername.value, 
+      phone: form.travellerphone.value,
+      bookingTime: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <form name="addTraveller" onSubmit={this.handleSubmit}>
+	    {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
+        {/* <input type="text" name="travellerid" placeholder="ID" />       */}
+        <input type="text" name="travellername" placeholder="Name" />
+        <input type = "text" name = "travellerphone" placeholder = "Phone" />
+        {/* <input type="text" name="travellerbookingtime" placeholder="Booking Time" /> */}
+        {/* <input type="text" name="travellerseat" placeholder="Seat" /> */}
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
 
 
 class Delete extends React.Component {
